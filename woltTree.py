3070b89c3332ae26290fa02cd1e9ec524d15fd29
Roy Schossberge <royschossberge@cs.huji.ac.il>
2022-08-20 12:02:42 +0300

    parsing to Data Frame adde menus file

---
+++ woltTree.py
@@ -0,0 +1,13 @@
+from simpleai.search import SearchProblem
+
+
+class WoltProblem(SearchProblem):
+
+    def cost(self, state, action, state2):
+        pass
+
+    def actions(self, state):
+        pass
+
+    def heuristic(self, state):
+        pass
