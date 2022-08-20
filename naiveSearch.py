79d29ba4f820722362c8b54090074554f947d557
brkram <barakramni@gmail.com>
2022-08-20 15:22:06 +0300

    updated loss function

--- naiveSearch.py
+++ naiveSearch.py
@@ -2,12 +2,13 @@ from itertools import combinations_with_replacement, product
 import pandas as pd
 from tqdm import tqdm
 import time
+import LossFunc
 
 CSV_RESTAURANTS     = "csv_wolt_restaurants_19-8-22.csv"
 CSV_MENUS           = "csv_wolt_menus_20-8-22.csv"
 NUMBER_OF_EATERS    = 3
 
-def naive_search(csv_restaurants = CSV_RESTAURANTS, csv_menus = CSV_MENUS, number_of_eaters = NUMBER_OF_EATERS):
+def naive_search( d1, d2, d3, csv_restaurants = CSV_RESTAURANTS, csv_menus = CSV_MENUS, number_of_eaters = NUMBER_OF_EATERS):
     # open the restaurants list csv in pandas df
     restaurants_list = pd.read_csv(CSV_RESTAURANTS, encoding="utf-8")
     menus_list = pd.read_csv(CSV_MENUS, encoding="utf-8")
@@ -33,9 +34,9 @@ def naive_search(csv_restaurants = CSV_RESTAURANTS, csv_menus = CSV_MENUS, numbe
 
         # calculate the score of each meal combination
         for permutation in tqdm(meals_product, position=1, desc=f'Restaurant - {idx}/{num_of_restaurants}', leave=False, ncols=80):
-
+            params = LossFunc.user_inputs_to_loss_function_inputs(d1, d2, d3, restaurant_name, permutation[0], permutation[1], permutation[2])
             # calculate the score of the permutation
-            score = 0 # TODO: update score function!
+            score = LossFunc.loss(*params)
             # if the score is better than the current best score, save the permutation and the score
             if chosen_restaurant_score is None or chosen_restaurant_score < score:
                 chosen_restaurant = restaurant_name
@@ -53,4 +54,11 @@ def naive_search(csv_restaurants = CSV_RESTAURANTS, csv_menus = CSV_MENUS, numbe
     print(f'Chosen restaurant score - {chosen_restaurant_score}')
     print(f'Chosen restaurant meals - {chosen_restaurant_meals}')
 
-naive_search()
+
+
+# example of usage
+if __name__ == "__main__":
+    d1 = [0, 0, 0, 0, 0, 80, 7, 0, ['burger'], 'sunday']
+    d2 = [0, 1, 0, 0, 0, 80, 5, 0, ['burger', 'hummus'], 'sunday']
+    d3 = [0, 0, 0, 0, 1, 80, 8, 0, ['salad'], 'sunday']
+    naive_search(d1, d2, d3)
