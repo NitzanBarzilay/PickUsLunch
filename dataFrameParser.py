5f56c4c25dfd2f32cce9e41250eefacb42e31369
Roy Schossberge <royschossberge@cs.huji.ac.il>
2022-08-20 11:42:59 +0300

    parsing to Data Frame adde menus file

--- dataFrameParser.py
+++ dataFrameParser.py
@@ -6,12 +6,24 @@ import pandas as pd
 
 
 class WoltParser:
-    def __init__(self, restaurants: List[Restaurant], init_file:bool =True):
+    def __init__(self, restaurants: List[Restaurant], init_files: bool = True):
         self.df = None
         self.restaurants = restaurants
-        if init_file:
+        if init_files:
             self.create_general_df()
+            self.crate_restaurants_df()
         self.file_name = "csv_wolt_19-8-22.csv"
+
+    def crate_restaurants_df(self):
+        headers = ["name", "price", "alcohol_percentage", "vegetarian", "GF", "image", "days", "spicy"]
+        with open(f"csv_wolt_menus_20-8-22.csv", "w", newline="", encoding='utf-8') as curr_file:
+            dw = csv.DictWriter(curr_file, delimiter=",", fieldnames=headers)
+            dw.writeheader()
+            for rest in self.restaurants:
+                for dish in rest.menu:
+                    line_dict = dish._asdict()
+                    line_dict["name"] = rest.name
+                    dw.writerow(line_dict)
 
     def create_general_df(self):
         headers = [
@@ -19,13 +31,14 @@ class WoltParser:
             "is active", "is valid", "kosher", "location lat long", "menu", "opening days", "prep estimation [minutes]"
             , "rating"
         ]
-        with open("csv_wolt_19-8-22.csv", "w", newline="", encoding='utf-8') as curr_file:
+        with open("csv_wolt_restaurants_19-8-22.csv", "w", newline="", encoding='utf-8') as curr_file:
             dw = csv.DictWriter(curr_file, delimiter=",", fieldnames=headers)
             dw.writeheader()
             for rest in self.restaurants:
                 line_dict = {"name": rest.name, "address": rest.address, "city": rest.city,
                              "delivery estimation [minutes]": rest.delivery_estimation,
-                             "delivery price": rest.delivery_price, "food categories": "---".join(rest.food_categories),
+                             "delivery price": rest.delivery_price,
+                             "food categories": "---".join(rest.food_categories),
                              "is active": rest.is_active, "is valid": rest.is_valid, "kosher": rest.kosher,
                              "location lat long": tuple(rest.location), "opening days": " ".join(rest.opening_days),
                              "prep estimation [minutes]": rest.prep_estimation, "rating": rest.rating, "menu": []}
@@ -37,8 +50,6 @@ class WoltParser:
 
     def read_df(self):
         self.df = pd.read_csv(self.file_name, encoding="utf-8")
-
-
 
 
 if __name__ == '__main__':
